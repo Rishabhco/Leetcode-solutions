c926cebf0f28bea1c5f8609b18e18905df2eb3ac
Rishabh Agrawal <rishabhagrawal2020@gmail.com>
2022-06-06 21:11:48 +0530

    Time: 14 ms (52.09%), Space: 7.4 MB (8.51%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -4,8 +4,7 @@ public:
         if(s.length()!=t.length()){
             return false;                      
         }                                       
-        map<char,int> mps;
-        map<char,int> mpt;
+        map<char,int> mps,mpt;
         for(int i=0;i<s.length();i++){
             mps[s.at(i)]++;
             mpt[t.at(i)]++;
