dbcb982ae784cf14ff94b84aa7df376eb4fc806d
Rishabh Agrawal <rishabhagrawal2020@gmail.com>
2022-08-05 12:04:18 +0530

    Time: 81 ms (5.30%), Space: 22.3 MB (55.64%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -12,37 +12,11 @@ public:
         // return 0;
         
         //upper_bound and first occurence
-        // for(int i=0;i<nums.size();i++){
-        //     if(upper_bound(nums.begin(),nums.end(),nums[i])-lower_bound(nums.begin(),nums.end(),nums[i])==1){
-        //         return nums[i];
-        //     }
-        // }
-        // return 0;
-        
-        if(nums.size() == 1)
-            return nums[0];
-        int l = 0, r = nums.size()-1;
-        while(l<=r){
-            int mid = (l+r)/2;
-            if(mid == 0 && mid+1<nums.size() && nums[mid+1] != nums[mid])
-                return nums[mid];
-            if(mid == nums.size()-1 && mid-1>=0 && nums[mid-1] != nums[mid])
-                return nums[mid];
-            if(mid>0 && nums[mid] != nums[mid-1] && mid<nums.size()-1 && nums[mid] != nums[mid+1])
-                return nums[mid];
-            if(mid & 1){
-                if(nums[mid] == nums[mid-1])
-                    l = mid+1;
-                else
-                    r = mid-1;
-            }
-            else{
-                if(nums[mid] == nums[mid+1])
-                    l = mid+1;
-                else
-                    r = mid-1;
+        for(int i=0;i<nums.size();i++){
+            if(upper_bound(nums.begin(),nums.end(),nums[i])-lower_bound(nums.begin(),nums.end(),nums[i])==1){
+                return nums[i];
             }
         }
-        return -1;
+        return 0;
     }
 };
