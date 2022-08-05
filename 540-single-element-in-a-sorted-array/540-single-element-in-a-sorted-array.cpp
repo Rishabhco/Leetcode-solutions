566fabbf1d8aac26c9ea52d8207d359ce6873d5f
Rishabh Agrawal <rishabhagrawal2020@gmail.com>
2022-08-05 11:46:46 +0530

    Time: 91 ms (5.30%), Space: 22.3 MB (55.64%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        map<int,int> mpp;
+        // map<int,int> mpp;
+        // for(int i=0;i<nums.size();i++){
+        //     mpp[nums[i]]++;        
+        // }    
+        // for(auto it:mpp){
+        //     if(it.second==1)
+        //         return it.first;
+        // }
+        // return 0;
+        
+        //upper_bound and first occurence
         for(int i=0;i<nums.size();i++){
-            mpp[nums[i]]++;        
-        }    
-        for(auto it:mpp){
-            if(it.second==1)
-                return it.first;
+            if(upper_bound(nums.begin(),nums.end(),nums[i])-lower_bound(nums.begin(),nums.end(),nums[i])==1){
+                return nums[i];
+            }
         }
         return 0;
     }
