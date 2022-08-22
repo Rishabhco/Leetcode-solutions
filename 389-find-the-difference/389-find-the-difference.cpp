a18f8a00a047b61aa417c1f795f596572fd98f60
Rishabh Agrawal <rishabhagrawal2020@gmail.com>
2022-08-22 21:49:43 +0530

    Time: 12 ms (6.90%), Space: 6.6 MB (88.16%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        for(int i=0;i<t.length();i++){
+            if(t[i]!=s[i]){
+                return t[i];
+            }
+        }
+        return t[0];
+    }
+};
