6bb010fffc371e85d521d84df05fa433e4b58133
Rishabh Agrawal <rishabhagrawal2020@gmail.com>
2022-09-29 14:39:46 +0530

    Time: 666 ms (24.33%), Space: 79.6 MB (23.39%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -5,11 +5,9 @@ public:
         vector<vector<int>> vec;
         for(int i=0;i<points.size();i++){
             pq.push(make_pair(sqrt((points[i][0]*points[i][0])+(points[i][1]*points[i][1])),points[i]));
-            cout<<sqrt((points[i][0]*points[i][0])+(points[i][1]*points[i][1]))<<endl;
         }
         for(int i=0;i<k;i++){
             vec.push_back(pq.top().second);
-            cout<<vec[i][0]<<vec[i][1]<<endl;
             pq.pop();
         }
         return vec;
